c102f6647003ffe658452e3dbb6a33f6b6401f63
mareeyah <you@example.comsmareeyah@ptnicec.ac.th>
2018-11-08 18:05:03 +0700

    send

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController,ActionSheetController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public alertCtrl: AlertController,
+    public actionSheet: ActionSheetController, 
+    public navCtr: NavController) {
   }
+
+  showActoin(){
+    const acSheet =this.actionSheet.create({
+      title:'Test Action',
+      buttons: [
+        {
+          text: 'Information',
+          role: 'Info',
+          handler: () => {
+            console.log('Info Clicked');
+          }
+        },
+      ]
+    });
+    acSheet.present();
+ }
+
+showAlert(){
+  const alert = this.alertCtrl.create({
+    title: 'confirm Answer!!',
+    subTitle : 'Mareeyah Sulong',
+   buttons: [
+     {
+       text: 'OK'
+     },
+     {
+       text: 'Cencel',
+       handler: () =>{
+         console.log("Cancel");
+       },
+      }
+    ]
+  });
+  alert.present();
+}
 
 }
